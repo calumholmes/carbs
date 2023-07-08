843d63c3be6e453f3acaea2692c9bddb8c9bde01
calumholmes <138944879+calumholmes@users.noreply.github.com>
2023-07-08 00:25:20 +0000

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# carbs
-CARBS is a minor script to slightly modify the look of Luke Smith's (lukesmith.xyz) LARBS (larbs.xyz)
+# What is CARBS?
+
+CARBS is an acronym that stands for "Calums Auto Ricing Bootstrapping Script"
+
+It's a minor modification to LARBS (created by Luke Smith, lukesmith.xyz, larbs.xyz)
+
+The script is meant to be run after running LARBS (larbs.xyz).It makes minor cosmetic changes and that's pretty much it.
+
+# Usage Case
+
+I made this primarily to simplify spreading this environment to other computers for myself. You may want to use it if:
+
+- You too have an irrational hatred of emojis
+
+- You like running scripts from random people on your computer
+
+- Some secret third reason
+
+# Installation
+
+Download the code from github and extract to a folder. Change directory to the folder and run the following:
+
+sh carbs.sh
+
+It should be basically instant.
+After you should renew dwm.
---
+++ carbs.sh
@@ -0,0 +1,31 @@
+# CARBS Install Script! (Minor changes to Luke Smiths LARBS)
+
+echo "Appending "Autumn" theme to ~/.config/x11/xresources"
+echo "Note: This assumes you have not already altered the xresources file."
+cat autumntheme >> ~/.config/x11/xresources
+echo "Making theme available on startup"
+cp -f xprofile ~/.config/x11/xprofile
+
+echo 'Replacing "sysact"'
+cp -f sysact ~/.local/bin/sysact
+
+echo "Replacing dwmblocks config"
+cp -f config.h ~/.local/src/dwmblocks/config.h
+
+echo "Replacing modified dwmblock modules"
+cp -f sb-volume ~/.local/bin/statusbar/
+cp -f sb-cpu ~/.local/bin/statusbar/
+cp -f sb-memory ~/.local/bin/statusbar/
+cp -f sb-battery ~/.local/bin/statusbar/
+cp -f sb-internet ~/.local/bin/statusbar/
+cp -f sb-clock ~/.local/bin/statusbar/
+cp -f sb-iplocate ~/.local/bin/statusbar/
+
+echo "Finishing up..."
+xrdb ~/.config/x11/xresources
+cp -f bg.jpg ~/Pictures/bg.jpg
+setbg ~/Pictures/bg.jpg
+
+echo "Done! Welcome to CARBS!"
+echo 'Please renew dwm by pressing super+backspace and selecting "Renew dwm"'
+echo "By the way you can delete this folder now."
