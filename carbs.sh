98dd4606ed2f0877a1d2d72905914225ac1a2205
calumholmes <138944879+calumholmes@users.noreply.github.com>
2023-07-08 18:47:43 +0000

    Add files via upload
    Added:
    - Functionality to add the official CARBS logo (tm) on boot

--- carbs.sh
+++ carbs.sh
@@ -8,29 +8,30 @@ echo "1. Autumn, 2. Petals, 3. Slate"
 echo -n "Choose: "
 read themechoice
 
-mkdir ~/Pictures
-
+mkdir ~/CARBS_Files
+mkdir ~/CARBS_Files/Wallpapers
 case $themechoice in
 1)
 	echo "Replacing ~/.config/x11/xresources"
 	cp -f autumntheme ~/.config/x11/xresources
 	echo "Setting background"
-	cp autumnbg.jpg ~/Pictures/
-	setbg ~/Pictures/autumnbg.jpg
+	cp autumnbg.jpg ~/CARBS_Files/Wallpapers
+	setbg ~/CARBS_Files/Wallpapers/autumnbg.jpg
 	;;
 2)
 	echo "Replacing ~/.config/x11/xresources"
 	cp -f petalstheme ~/.config/x11/xresources
 	echo "Setting background"
-	cp petalsbg.jpg ~/Pictures/
-	setbg ~/Pictures/petalsbg.jpg
+	cp petalsbg.jpg ~/CARBS_Files/Wallpapers
+
+	setbg ~/CARBS_Files/Wallpapers/petalsbg.jpg
 	;;
 3)
 	echo "Replacing ~/.config/x11/xresources"
 	cp -f slatetheme ~/.config/x11/xresources
 	echo "Setting background"
-	cp slatebg.jpg ~/Pictures/
-	setbg ~/Pictures/slatebg.jpg
+	cp slatebg.jpg ~/CARBS_Files/Wallpapers
+	setbg ~/CARBS_Files/Wallpapers/slatebg.jpg
 	;;
 *)
 	echo "Not valid. Skipping..."
@@ -51,7 +52,7 @@ cd $SCRIPT_DIR
 echo "Installing geoip"
 sudo pacman -S --noconfirm geoip
 
-echo "Replacing modified dwmblock modules"
+echo "Replacing old dwmblock modules"
 cp -f sb-volume ~/.local/bin/statusbar/
 cp -f sb-cpu ~/.local/bin/statusbar/
 cp -f sb-memory ~/.local/bin/statusbar/
@@ -60,8 +61,27 @@ cp -f sb-internet ~/.local/bin/statusbar/
 cp -f sb-clock ~/.local/bin/statusbar/
 cp -f sb-iplocate ~/.local/bin/statusbar/
 
-echo "Finishing up..."
+echo "Finishing up theming..."
 xrdb ~/.config/x11/xresources
+
+echo -n "Add the CARBS motd on startup? Say now if you've aready done it. (Y/n): "
+read askmotd
+case $askmotd in
+	y)
+		mkdir "/home/$(whoami)/CARBS_Files/MOTD"
+		runcommand="sh /home/$(whoami)/CARBS_Files/MOTD/motd.sh"
+		echo "$runcommand"
+		touch "/home/$(whoami)/CARBS_Files/MOTD/runmotd"
+		sudo echo $runcommand > "/etc/rc.local"
+		cat "/home/$(whoami)/CARBS_Files/MOTD/runmotd"
+		cp -f motd.sh /home/$(whoami)/CARBS_Files/MOTD/motd.sh
+		echo "cat /home/$(whoami)/CARBS_Files/MOTD/carbs.txt" >> motd.sh
+		cp -f carbs.txt /home/$(whoami)/CARBS_Files/MOTD/carbs.txt
+		;;
+	*)
+		echo "oops"
+		;;
+esac
 
 echo "Done! Welcome to CARBS!"
 echo 'Please renew dwm by pressing super+backspace and selecting "Renew dwm"'
